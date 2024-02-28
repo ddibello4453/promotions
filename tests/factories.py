2ca5b1fb2336a7468c9509d15fe3ab370885199d
Amanda Aulia <amanda.hanggoro@gmail.com>
2024-02-28 19:50:10 +0000

    Added Promotions model

--- tests/factories.py
+++ tests/factories.py
@@ -3,16 +3,16 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from service.models import Promotions
 
 
-class YourResourceModelFactory(factory.Factory):
+class PromotionsFactory(factory.Factory):
     """Creates fake pets that you don't have to feed"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = YourResourceModel
+        model = Promotions
 
     id = factory.Sequence(lambda n: n)
     name = factory.Faker("first_name")
